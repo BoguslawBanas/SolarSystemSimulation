6abb7077f173d3ca145188017a14fd9b377136de
Bogusław Banaś <bogus.banas@interia.pl>
2025-11-19 14:18:01 +0100

    Changes in the structure of the project.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#define RAYGUI_IMPLEMENTATION
+
+#include "../header_files/simulation.h"
+
+int main(){
+    Simulation *simulation=new Simulation("simulation_settings.conf");
+    // Simulation *simulation=new Simulation("..\\simulation_settings.conf");
+
+    while(simulation->getState()!=EXIT){
+        simulation->calcLogic();
+        simulation->drawSimulation();
+    }
+
+    delete simulation;
+    return 0;
+}
